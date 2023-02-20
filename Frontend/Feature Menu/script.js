b35356c6f6bdd1db6e8546470180d132b7312f37
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-02-20 11:49:51 +0530

    Add files via upload

--- Frontend/Feature Menu/script.js
+++ Frontend/Feature Menu/script.js
@@ -1,3 +1,15 @@
 function logout(){
-    window.open("../Landing Page/index.html")
+    window.open("../Landing Page/index.html");
+}
+
+function note() {
+    window.open("../Note making/index.html");
+}
+
+function timer() {
+    window.open("../Timer/index.html");
+}
+
+function todolist() {
+    window.open("../To-Do List/index.html");
 }
