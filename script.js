df1d4e99549ca1ba2b42ec5253087f0de76fac3a
Akshat Trivedi <akshattrivedi32@gmail.com>
2023-10-23 15:10:09 +0530

    created back-to-top button

--- script.js
+++ script.js
@@ -48,3 +48,24 @@ window.addEventListener('scroll', function () {
         });
     }
 });
+
+//js file for the back to top button
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
